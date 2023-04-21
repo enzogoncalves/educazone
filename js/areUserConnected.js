6fac8a5592414ad44356da1594164d0cca89a9b4
enzogoncalves <enzomateusgonc@gmail.com>
2023-04-20 22:19:37 -0300

    profile and edit profile page

--- js/areUserConnected.js
+++ js/areUserConnected.js
@@ -1,4 +1,7 @@
-import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
+import {
+	getAuth,
+	onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js"
 import {
 	getDatabase,
 	ref,
@@ -8,41 +11,35 @@ import {
 const auth = getAuth()
 
 onAuthStateChanged(auth, (user) => {
-    if (user) {
-        // Caso tenha um usuário, pego o id dele e busco os dados no realtime database do firebase
+	if (user) {
+		const db = getDatabase()
+		const dbRef = ref(db)
 
-        const db = getDatabase()
-        const dbRef = ref(db)
-
-        onValue(dbRef, (snapshot) => {
-        loadUserData(snapshot.val(), user)
-    })
-        // a função onValue roda toda vez que o banco de dados na referência do usuário atual se modifica (quando adiciona, edita ou exclui o usuário)
-
-    } else {
-        // Caso não tiver um usuário conectado, ele vai para a página de login/cadastro
-        window.location.pathname = "/"
-    }
+		createHeader(user.photoURL)
+	} else {
+		// Caso não tiver um usuário conectado, ele vai para a página inicial
+		window.location.pathname = "/"
+	}
 })
 
-function loadUserData(snapshot, user) {
-    let userData;
-    
-    for (const i in snapshot) {
-        for (const j in snapshot[i]) {
-            if(j === user.uid) userData = snapshot[i][j];
-        }
-    }
-    createHeader(user.photoUEL)
-    console.log(user)
+export default function getUserData(snapshot, userUid) {
+	let userData
+
+	for (const i in snapshot) {
+		for (const j in snapshot[i]) {
+			if (j === userUid) userData = snapshot[i][j]
+		}
+	}
+
+	return userData
 }
 
 function createHeader(imageUrl) {
-    const body = document.querySelector('body')
-    const header = document.createElement('header')
+	const body = document.querySelector("body")
+	const header = document.createElement("header")
 
-    header.innerHTML = `
-        <header>
+	header.innerHTML = `
+        <header id="user-header">
             <nav>
                 <a href="/" id="title">Educa<span>Zone</span></a>
                 <ul role="navigation">
@@ -53,6 +50,6 @@ function createHeader(imageUrl) {
         </header>
     `
 
-    body.childNodes[1].remove()
-    body.appendChild(header)
+	body.childNodes[1].remove()
+	body.appendChild(header)
 }
--- js/profile.js
+++ js/profile.js
@@ -21,9 +21,6 @@ onAuthStateChanged(auth, (user) => {
 		const dbRef = ref(db)
 
 		onValue(dbRef, (snapshot) => {
-			// snapshot.val() -> dados do usuário no banco de dados
-			// user -> dados do usuário no authentication
-
 			dbUserData = getUserData(snapshot.val(), user.uid)
 			handleUserData(dbUserData, user)
 		})
@@ -40,13 +37,7 @@ const site_label = document.querySelector("#site")
 
 // função para carregar o dado do usuário na página
 function handleUserData(dbUserData, authUserData) {
-	fullname_label.value = authUserData.displayName
-	email_label.value = authUserData.email
+	fullname_label.textContent = authUserData.displayName
+	email_label.textContent = authUserData.email
 	console.log(authUserData)
 }
-
-// eu aconselho a fazer um update no banco de dados de todos os campos exceto: nome completo, senha e email, pois terá que usar uma função para cada um do próprio firebase.
-
-document.querySelector("form").addEventListener("submit", (e) => {
-	e.preventDefault() // impede que o evento padrão aconteça
-})
