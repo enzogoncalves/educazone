daf751b68b460a6922277ec4c47da6109dbb9ae7
enzogoncalves <enzomateusgonc@gmail.com>
2023-05-09 17:05:41 -0300

    failed

--- src/server.js
+++ src/server.js
@@ -5,11 +5,12 @@ const route = require("./route")
 
 server.set("view engine", "ejs") // avisar qual engine será usada
 
-server.use(express.static(__dirname + "/public/"))
+server.use(express.static("public"))
 
 server.set("views", path.join(__dirname, "views"))
 
 server.use(express.urlencoded({ extended: true })) // habilitar o uso do ejs no html
 
 server.use(route)
+
 server.listen(3300, () => console.log("Ouvindo porta 3300"))
