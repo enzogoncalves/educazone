89687455a4389d95ca17bf6ee542d932c94898ca
enzogoncalves <enzomateusgonc@gmail.com>
2023-06-15 18:41:12 -0300

    Dashboard do professor concluído

--- public/js/professorDashboard.js
+++ public/js/professorDashboard.js
@@ -4,7 +4,7 @@ import { getFirestore, getDocs, collection, query, where } from "https://www.gst
 const auth = getAuth()
 
 import { app } from "./initializeFirebase.js"
-import { redirectToLoginPage } from "./modules.js"
+import { createProfilePicture, redirectToLoginPage } from "./modules.js"
 
 const firestoreDb = getFirestore(app)
 
@@ -25,37 +25,107 @@ onAuthStateChanged(auth, async authUser => {
 
 		querySnapshot.forEach(doc => {
 			const studentData = doc.data()
-			students.push({...studentData, studentId: doc.id})
+			students.push({ ...studentData, studentId: doc.id })
+		})
+
+		const tasksRef = collection(firestoreDb, "tasks")
+		const tasksSnapshot = await getDocs(query(tasksRef, where("professorId", "==", userId)))
+
+		const tasksStatus = {
+			total: 0,
+			corrigida: 0,
+			visualizado: 0,
+			falta_entregar: 0,
+			falta_corrigir: 0,
+		}
+
+		tasksSnapshot.forEach(task => {
+			const professorTask = task.data()
+			tasksStatus.total++
+
+			if (professorTask.delivered && professorTask.status == "Entregue") {
+				tasksStatus.falta_corrigir++
+			} else if (professorTask.status == "Corrigida") {
+				tasksStatus.corrigida++
+			} else if (professorTask.status == "Visualizado") {
+				tasksStatus.visualizado++
+			} else if (!professorTask.delivered) {
+				tasksStatus.falta_entregar++
+			}
 		})
 
 		document.querySelector(".page-skeleton").classList.remove("active")
 
 		displayStudentsTable(students) // Display students in the HTML table
+		displayTasks(tasksStatus)
 		getPaymentDate(userId)
 	} else {
 		redirectToLoginPage()
 	}
 })
 
+function displayTasks(tasksStatus) {
+	const tasks = document.querySelector("#activities > div")
+
+	tasks.innerHTML += `
+		<div>
+		<i class="icon-download"></i>
+		<p>${tasksStatus.total}</p>
+		<span>Total</span>
+		</div>
+		<div>
+		<i class="icon-upload"></i>
+		<p>${tasksStatus.falta_entregar}</p>
+		<span>Faltam entregar</span>
+		</div>
+		<div>
+		<i class="icon-document-time"></i>
+		<p>${tasksStatus.falta_corrigir}</p>
+		<span>Correção Pendente</span>
+		</div>
+		<div>
+		<i class="icon-thumbs-up"></i>
+		<p>${tasksStatus.corrigida}</p>
+		<span>Corrigda</span>
+		</div>
+	`
+}
+
 function displayStudentsTable(students) {
-	const tbody = document.getElementById("tbody")
+	const studentsContainer = document.querySelector("#myStudents")
+
+	if (students.length == 0) {
+		studentsContainer.innerHTML += `
+			<p>Você não possui nenhum aluno registrado</p>
+		`
+
+		return
+	}
 
 	students.forEach(student => {
-		const row = tbody.insertRow()
+		const studentDiv = document.createElement("div")
 
-		const pictureCell = row.insertCell()
-		pictureCell.textContent = "--FOTO--"
+		if (student.pictureUrl) {
+			studentDiv.innerHTML += `
+				<img src="${student.pictureUrl}" alt="Foto de perfil"/>
+			`
+		} else {
+			studentDiv.appendChild(createProfilePicture(student.firstName, student.lastName))
+		}
 
-		const nameCell = row.insertCell()
-		nameCell.textContent = student.firstName
+		studentDiv.innerHTML += `
+			<p class="studentName">${student.firstName} ${student.lastName}</p> 
+		`
 
-		const buttonCell = row.insertCell()
-		const button = document.createElement("button")
-		button.textContent = "Acessar aluno"
-		button.addEventListener("click", () => {
+		const accessStudentBtn = document.createElement("button")
+		accessStudentBtn.textContent = "Acessar aluno"
+		accessStudentBtn.addEventListener("click", () => {
 			window.location = `professor/${student.studentId}/`
 		})
-		buttonCell.appendChild(button)
+
+		studentDiv.appendChild(accessStudentBtn)
+
+		studentsContainer.appendChild(studentDiv)
 	})
 }
 
@@ -76,36 +146,55 @@ async function getPaymentDate(teacherId) {
 	let received = 0
 	let toReceive = 0
 	let totalAmountMonth = 0
-
-	const totalHTML = document.getElementById("total")
-	const receivedAmountHTML = document.getElementById("recebido")
-	const toReceiveHTML = document.getElementById("upcomingPayment")
+	let late = 0
 
 	try {
+		const financies = document.querySelector("#financies")
+
 		const snapshot = await getDocs(paymentCollection)
-		snapshot.forEach(doc => {
-			const paymentDate = doc.data().date
-			const paymentAmount = Number(doc.data().amount)
-			const firebaseFormatted = new Date(formatDate(paymentDate))
 
-			if (currentDate < firebaseFormatted) {
-				toReceive = paymentAmount
-			} else if (currentDate >= firebaseFormatted) {
-				received = paymentAmount
-			}
+		if (snapshot.empty) {
+			financies.innerHTML += `
+				<p>Nenhum pagamento encontrado</p>
+			`
+		} else {
+			snapshot.forEach(doc => {
+				const paid = doc.data()
+				const paymentDate = doc.data().date
+				const paymentAmount = doc.data().amount
+				const firebaseFormatted = new Date(formatDate(paymentDate))
 
-			totalAmountMonth += received + toReceive
-		})
+				if (currentDate < firebaseFormatted) {
+					toReceive += paymentAmount
+					totalAmountMonth += paymentAmount
+				} else if (currentDate >= firebaseFormatted && paid) {
+					received += paymentAmount
+					totalAmountMonth += paymentAmount
+				} else if (currentDate >= firebaseFormatted && !paid) {
+					late += paymentAmount
+					totalAmountMonth += paymentAmount
+				}
+			})
 
-		const totalText = document.createTextNode(totalAmountMonth)
-		const receivedText = document.createTextNode(received)
-		const toReceiveText = document.createTextNode(toReceive)
-
-		totalHTML.appendChild(totalText)
-
-		receivedAmountHTML.appendChild(receivedText)
-
-		toReceiveHTML.appendChild(toReceiveText)
+			financies.innerHTML += `
+				<div id="total">
+					<h5>Total</h5>
+					<p>${totalAmountMonth}</p>
+				</div>
+				<div class="recebido" id="recebido">
+						<h5>Recebidos</h5>
+						<p>${received}</p>
+				</div>
+				<div class="faltante" id="upcomingPayment">
+						<h5>A receber até o fim do mês</h5>
+						<p>${toReceive}</p>
+				</div>
+				<div class="late" id="late">
+						<h5>Atrasado</h5>
+						<p>${late}</p>
+				</div>
+			`
+		}
 	} catch (error) {
 		console.error("Error getting payment documents:", error)
 	}
