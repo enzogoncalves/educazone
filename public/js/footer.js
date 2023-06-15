91c5cbde3c5e2e003fa76e247d3bdcf965f115b5
enzogoncalves <enzomateusgonc@gmail.com>
2023-06-15 14:42:50 -0300

    footer changes

--- public/js/footer.js
+++ public/js/footer.js
@@ -1,22 +1,14 @@
 function footer() {
-	const footer = document.createElement('footer')
+	const footer = document.createElement("footer")
 
 	footer.innerHTML = `
-		<ul class="footer-list">
-			<a href="#">Perguntas Frequentes</a>
-			<a href="#">Professores</a>
-			<a href="#">Atendimento ao cliente</a>
-			<a href="#">Acessar Dashboard</a>
-		</ul>
-		<div>
 			<p>Todos os direitos reservados © a Enzo Mateus e Ulisses Duo</p>
 			<a href="#termos">Termos de Serviço</a>
 			|
 			<a href="#política">Política de Privacidade</a>
-		</div>
 	`
 
-	const body = document.querySelector('body')
+	const body = document.querySelector("body")
 	body.appendChild(footer)
 }
 
